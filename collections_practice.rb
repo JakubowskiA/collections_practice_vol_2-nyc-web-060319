25e9d58c92b35c41a6bd18c8cf8a0211bfcef204
Ariel J <arieljakubowski@gmail.com>
2019-05-29 17:47:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,4 @@
+require "pry"
 def begins_with_r(tools)
 tools.all? do |tool|
 tool.downcase.chr == "r"
@@ -22,4 +23,11 @@ def remove_non_strings(array)
   array.delete_if do |string|
   string.is_a?(String) == false
   end
+end
+
+def count_elements(array)
+     array.uniq.each {|i| count = 0
+        array.each {|i2| if i2 == i then count += 1 end}
+        i[:count] = count}
+        binding.pry
 end
